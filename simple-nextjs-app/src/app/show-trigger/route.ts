8eb9b2c95677c31fc4fabb9333980db7d0235c5b
chedieck <estevao@lobo.dev.br>
2024-02-19 11:23:18 -0300

    feat: using POST signature to verify authenticity of POST request

--- simple-nextjs-app/src/app/show-trigger/route.ts
+++ simple-nextjs-app/src/app/show-trigger/route.ts
@@ -1,10 +1,35 @@
+import crypto from 'crypto'
+
+interface Signature {
+  payload: string
+  signature: string
+}
+
+const verifySignature = (pubKey: string, signature: Signature) => {
+  const pubKeyBuffer = Buffer.from(pubKey, 'hex')
+  const publicKey = crypto.createPublicKey({ key: pubKeyBuffer, format: 'der', type: 'spki' });
+  return crypto.verify(
+    null,
+    Buffer.from(signature.payload),
+    publicKey,
+    Buffer.from(signature.signature, 'hex')
+  )
+}
+
 export const POST = async (req: Request): Promise<Response> => {
   const values = await req.json()
-  const { secret, ...clearValues } = values
-  if (secret === process.env.SECRET) {
-    console.log("Received valid POST request:", clearValues)
+  const signature = values.signature
+  const publicKey = process.env.PUBLIC_KEY
+  if (publicKey === undefined) {
+    throw new Error("Missing PUBLIC_KEY environment variable.")
+  }
+  if (signature === undefined) {
+    throw new Error("Missing <signature> variable on POST request body.")
+  }
+  if (verifySignature(publicKey, values.signature)) {
+    console.log("Received VALID POST request:", values)
   } else {
-    console.log("Received invalid POST request:", values)
+    console.log("Received INVALID POST request:", values)
   }
 
   return Response.json({status: 200})
